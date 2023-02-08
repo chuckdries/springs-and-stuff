ca21dfcd11db7512a186dd161e9f593a2a9539ee
Chuck Dries <chuck@chuckdries.com>
2023-02-07 18:15:20 -0800

    bring in react-aria switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import { useState } from "react";
+import { Switch } from "./Switch";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <div className="flex flex-col gap-5 items-center">
-      <button
-        className="border border-white hover:bg-slate-600 rounded-lg p-2"
-        type="button"
-        onClick={() => setCount(count + 1)}
-      >
-        {count} + 1
-      </button>
-      <div className="w-[600px] border-2 border-green-400 rounded-xl p-[20px]">
-        <div
-          className="relative h-[100px] w-[200px] flex items-center justify-center bg-red-500 rounded-lg"
-          style={{
-            transform: `translateX(${count % 2 === 0 ? '0px' : '355px'})`
-          }}
-        >
-          count is {count % 2 === 0 ? "even" : "odd"}
-        </div>
-      </div>
+      <Switch isSelected={darkMode} onChange={setDarkMode}>Dark Mode</Switch>
     </div>
   );
 }
